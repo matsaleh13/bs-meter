32075da0aedce4fcb6ddb058b7952b600c77743e
Matthew Walker <matsaleh13@live.com>
2015-09-24 18:42:28 -0500

    Implement real async support using StreamReader.ReadAsync()

--- AnalysisLib/CharacterAnalyzer.cs
+++ AnalysisLib/CharacterAnalyzer.cs
@@ -9,13 +9,14 @@ namespace AnalysisLib
     /// </summary>
     public class CharacterAnalyzer : IAnalyzer
     {
+
         /// <summary>
         /// Synchronous method to analyze the data stream.
         /// </summary>
         /// <param name="data">Data stream to analyze.</param>
         /// <param name="blockSize">Max number of characters to read at a time.</param>
         /// <returns>Result of the analysis.</returns>
-        public IAnalyzerResult Analyze(Stream data, int blockSize=1024)
+        public IAnalyzerResult Analyze(Stream data, int blockSize = 1024)
         {
             var result = new CharacterAnalyzerResult();
             var buffer = new char[blockSize];
@@ -34,33 +35,15 @@ namespace AnalysisLib
                     {
                         var c = buffer[ix];
 
-                        IRepeatCounter currentCounter = null;
-                        var currentCharType = CharacterType.Invalid;
-
-                        if (char.IsPunctuation(c))
-                        {
-                            currentCounter = result.Punctuation;
-                            currentCharType = CharacterType.Punctuation;
-                        }
-                        else if (char.IsWhiteSpace(c))
-                        {
-                            currentCounter = result.Whitespace;
-                            currentCharType = CharacterType.Whitespace;
-                        }
-                        else if (char.IsUpper(c))
-                        {
-                            currentCounter = result.UpperCase;
-                            currentCharType = CharacterType.UpperCase;
-                        }
-                        else
-                        {
-                            currentCounter = result.Other;
-                            currentCharType = CharacterType.Other;
-                        }
+                        var currentCharType = CharacterTypeUtils.GetCharType(c);
+                        var currentCounter = result.GetCounter(currentCharType);
 
                         currentCounter.Counter.Increment();
                         if (currentCharType == lastCharType) currentCounter.RepeatCounter.Increment();
+
+                        lastCharType = currentCharType;
                     }   // for
+
                 }   // while
 
             }
@@ -70,11 +53,44 @@ namespace AnalysisLib
 
         /// <summary>
         /// Asynchronous method to analyze the data stream.
-        /// TODO: Full async internally, calling StreamReader.ReadAsync() or similar.
+        /// TODO: More DRY here please.
         /// </summary>
         /// <param name="data">Data stream to analyze.</param>
         /// <param name="blockSize">Max number of characters to read at a time.</param>
         /// <returns>Result of the analysis.</returns>
-        public async Task<IAnalyzerResult> AnalyzeAsync(Stream data, int blockSize = 1024) => await Task.Run(() => Analyze(data, blockSize));
+        public async Task<IAnalyzerResult> AnalyzeAsync(Stream data, int blockSize = 1024)
+        {
+            var result = new CharacterAnalyzerResult();
+            var buffer = new char[blockSize];
+
+            using (var reader = new StreamReader(data))
+            {
+                var lastCharType = CharacterType.Invalid;
+
+                while (!reader.EndOfStream)
+                {
+                    var count = await reader.ReadAsync(buffer, 0, blockSize).ConfigureAwait(false);
+
+                    result.CharacterCount.Increment(count);
+
+                    for (int ix = 0; ix < count; ++ix)
+                    {
+                        var c = buffer[ix];
+
+                        var currentCharType = CharacterTypeUtils.GetCharType(c);
+                        var currentCounter = result.GetCounter(currentCharType);
+
+                        currentCounter.Counter.Increment();
+                        if (currentCharType == lastCharType) currentCounter.RepeatCounter.Increment();
+
+                        lastCharType = currentCharType;
+                    }   // for
+
+                }   // while
+
+            }
+
+            return result;
+        }
     }
 }
--- AnalysisLib/CharacterAnalyzerResult.cs
+++ AnalysisLib/CharacterAnalyzerResult.cs
@@ -4,6 +4,24 @@ using System.Threading;
 
 namespace AnalysisLib
 {
+    class InvalidCharacterTypeException : ApplicationException
+    {
+        public InvalidCharacterTypeException()
+        {
+
+        }
+
+        public InvalidCharacterTypeException(string message)
+        : base(message)
+        {
+        }
+
+        public InvalidCharacterTypeException(string message, Exception inner)
+        : base(message, inner)
+        {
+        }
+    }
+
     public enum CharacterType
     {
         Invalid,
@@ -13,10 +31,36 @@ namespace AnalysisLib
         Other
     }
 
+    public class CharacterTypeUtils
+    {
+        public static CharacterType GetCharType(char c)
+        {
+            var currentCharType = CharacterType.Invalid;
+
+            if (char.IsPunctuation(c))
+            {
+                currentCharType = CharacterType.Punctuation;
+            }
+            else if (char.IsWhiteSpace(c))
+            {
+                currentCharType = CharacterType.Whitespace;
+            }
+            else if (char.IsUpper(c))
+            {
+                currentCharType = CharacterType.UpperCase;
+            }
+            else
+            {
+                currentCharType = CharacterType.Other;
+            }
+
+            return currentCharType;
+        }
+    }
+
 
     public class CharacterAnalyzerResult : IAnalyzerResult
     {
-
         public CharacterAnalyzerResult()
         {
             CharacterCount = new CharacterCounter();
@@ -40,5 +84,21 @@ namespace AnalysisLib
 
         public IRepeatCounter Whitespace { get; }
 
+        public IRepeatCounter GetCounter(CharacterType charType)
+        {
+            switch (charType)
+            {
+                case CharacterType.Punctuation:
+                    return Punctuation;
+                case CharacterType.Whitespace:
+                    return Whitespace;
+                case CharacterType.UpperCase:
+                    return UpperCase;
+                case CharacterType.Other:
+                    return Other;
+                default:
+                    throw new InvalidCharacterTypeException(string.Format("Invalid character type {0}", charType));
+            }
+        }
     }
 }
