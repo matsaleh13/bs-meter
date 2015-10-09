a3d4e346ce799d419416db8de11898cec4e59752
Matthew Walker <matsaleh13@live.com>
2015-10-09 18:58:02 -0500

    WIP: starting with creation of model classes and lightweigh Repository pattern implementation for Redis.

---
+++ DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,32 @@
+﻿using DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Interfaces
+{
+    public interface IRepository<TEntity>
+    {
+        IQueryable<TEntity> Query();
+        IEnumerable<TEntity> Find(Func<TEntity, bool> exp);
+        TEntity FirstOrDefault(Func<TEntity, bool> exp);
+
+        void Delete(TEntity entity);
+        void Add(TEntity entity);
+        void Save();
+    }
+
+    public interface IRepositoryAsync<TEntity>
+    {
+        IQueryable<TEntity> QueryAsync();
+        IEnumerable<TEntity> FindAsync(Func<TEntity, bool> exp);
+        TEntity FirstOrDefaultAsync(Func<TEntity, bool> exp);
+
+        void DeleteAsync(TEntity entity);
+        void AddAsync(TEntity entity);
+        void SaveAsync();
+    }
+
+}
---
+++ DataAccess/RedisRepository.cs
@@ -0,0 +1,48 @@
+﻿using DataAccess.Interfaces;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataAccess
+{
+    public abstract class RedisRepositoryAsync<TEntity> : IRepositoryAsync<TEntity>
+    {
+        IDatabase _db;
+
+        protected RedisRepositoryAsync(IDatabase db)
+        {
+            _db = db;
+        }
+
+        public IQueryable<TEntity> QueryAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> FindAsync(Func<TEntity, bool> exp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity FirstOrDefaultAsync(Func<TEntity, bool> exp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
