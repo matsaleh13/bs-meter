cee57c5527b0632c0480a4ebe27e8f6777dd0d13
Matthew Walker <matsaleh13@live.com>
2015-10-30 14:12:13 -0500

    Add test of GlobalConfig singleton.

--- Common.Tests/GlobalConfigTests.cs
+++ Common.Tests/GlobalConfigTests.cs
@@ -14,5 +14,19 @@ namespace Common.Tests
             Assert.IsNotNullOrEmpty(config.Corpus.Redis.ConnectionString);
             Assert.AreEqual("{HOST}:6379,allowAdmin=true,ssl=false,connectTimeout=5000,database=0,password=null", config.Corpus.Redis.ConnectionString);
         }
+
+        [Test]
+        public void GlobalConfigSingletonTest()
+        {
+            var config = GlobalConfig.Instance;
+
+            Assert.IsNotNull(config.Corpus);
+            Assert.IsNotNullOrEmpty(config.Corpus.Redis.ConnectionString);
+            Assert.AreEqual("{HOST}:6379,allowAdmin=true,ssl=false,connectTimeout=5000,database=0,password=null", config.Corpus.Redis.ConnectionString);
+
+            Assert.AreSame(config, GlobalConfig.Instance);
+            Assert.AreNotSame(config, new GlobalConfig());
+        }
+
     }
 }
