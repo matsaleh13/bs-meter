f4540eed32906c5d36a161b2205e5ddf7ecbe527
Matthew Walker <matsaleh13@live.com>
2015-11-01 18:25:09 -0600

    Fix missing SET statement.

--- docker/start_machine.bat
+++ docker/start_machine.bat
@@ -13,7 +13,7 @@ CALL %MACHINE%.bat
 
 REM Get the IP address and store it in an environment file
 REM NOTE: This is a really ugly hack (IMO)
-TEMPFILE=%MACHINE%-ip.tmp
+SET TEMPFILE=%MACHINE%-ip.tmp
 docker-machine ip %MACHINE% > %TEMPFILE%
 SET /p REDIS_HOST=<%TEMPFILE%
 DEL %TEMPFILE%
