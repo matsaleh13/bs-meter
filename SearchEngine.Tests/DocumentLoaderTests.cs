e30c7ebf9fe3008fc501ee9888f0f0d011662e0a
Matthew Walker <matsaleh13@live.com>
2015-11-05 22:34:47 -0600

    Dammit, why isn't Git auto-adding files? Grr.

---
+++ SearchEngine.Tests/DocumentLoaderTests.cs
@@ -0,0 +1,205 @@
+﻿using SearchEngine;
+using NUnit.Framework;
+using FakeItEasy;
+using DataAccess.Interfaces;
+using AnalysisModel;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+using System;
+using Mock4Net.Core;
+
+namespace SearchEngine.Tests
+{
+    [TestFixture]
+    public class DocumentLoaderTests
+    {
+        const string _testFile = "data.txt";
+
+        public void CreateFile(string path, string contents)
+        {
+            File.WriteAllText(path, contents);
+        }
+
+        public void DeleteFile(string path)
+        {
+            File.Delete(path);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            DeleteFile(_testFile);
+        }
+
+
+        [TestCase(16)]
+        [TestCase(256)]
+        [TestCase(1024)]
+        [TestCase(4096)]
+        [TestCase(16384)]
+        public void LoadAsyncStreamTest(int blockSize)
+        {
+            IRepositoryAsync<Document> repo = A.Fake<IRepositoryAsync<Document>>();
+            A.CallTo(() => repo.AddAsync(A<Document>.Ignored)).Returns(Task.FromResult(true));
+
+            var loader = new DocumentLoaderAsync(repo, blockSize);
+
+            var data = new MemoryStream(Encoding.UTF8.GetBytes(TestDocument));
+            var awaiter = Task.Run(async () => await loader.LoadAsync(data, "text/plain").ConfigureAwait(false));
+            var result = awaiter.Result;
+
+            Assert.IsTrue(result);
+        }
+
+        [TestCase(16)]
+        [TestCase(256)]
+        [TestCase(1024)]
+        [TestCase(4096)]
+        [TestCase(16384)]
+        public void LoadAsyncUriFileTest(int blockSize)
+        {
+            CreateFile(_testFile, TestDocument);
+
+            IRepositoryAsync<Document> repo = A.Fake<IRepositoryAsync<Document>>();
+            A.CallTo(() => repo.AddAsync(A<Document>.Ignored)).Returns(Task.FromResult(true));
+
+            var loader = new DocumentLoaderAsync(repo, blockSize);
+
+            var testPath = Path.GetFullPath(_testFile);
+            var awaiter = Task.Run(async () => await loader.LoadAsync(new Uri(string.Format("file:///{0}", testPath))).ConfigureAwait(false));
+            var result = awaiter.Result;
+
+            Assert.IsTrue(result);
+        }
+
+        [TestCase(16)]
+        [TestCase(256)]
+        [TestCase(1024)]
+        [TestCase(4096)]
+        [TestCase(16384)]
+        public void LoadAsyncUriWebTest(int blockSize)
+        {
+            var server = FluentMockServer.Start();
+            server
+                .Given(
+                    Requests.WithUrl(string.Format("/{0}", _testFile))
+                )
+                .RespondWith(
+                    Responses
+                        .WithStatusCode(200)
+                        .WithBody(TestDocument)
+                );
+
+            IRepositoryAsync<Document> repo = A.Fake<IRepositoryAsync<Document>>();
+            A.CallTo(() => repo.AddAsync(A<Document>.Ignored)).Returns(Task.FromResult(true));
+
+            var loader = new DocumentLoaderAsync(repo, blockSize);
+
+            var awaiter = Task.Run(async () => await loader.LoadAsync(new Uri(string.Format("http://localhost:{0}/{1}", server.Port, _testFile))).ConfigureAwait(false));
+            var result = awaiter.Result;
+
+            Assert.IsTrue(result);
+        }
+
+
+        #region Test Data
+
+        string TestDocument = @"PLEASE PASS THIS ALONG
+
+ 
+Warren Buffet is asking each addressee to forward this email to a minimum of twenty people on their address list; in turn ask each of those to do likewise.
+In three days, most people in The United States of America will have the message. This is one idea that really should be passed around.
+When someone does something good, applaud! Begin forwarded
+
+ 
+I REPORT, YOU DECIDE.
+
+ 
+The BUFFETT Rule
+
+ 
+We must support this - pass it on and let’s see if these idiots understand what people pressure is all about.
+
+ 
+Salary of retired US Presidents .. . . . .. . . . . .. . $180,000 FOR LIFE
+
+ 
+Salary of House/Senate members .. . . . .. . . . $174,000 FOR LIFE This is stupid
+
+ 
+Salary of Speaker of the House .. . . . .. . . . . $223,500 FOR LIFE This is really stupid
+
+ 
+Salary of Majority/Minority Leaders . . .. . . . . $193,400 FOR LIFE Ditto last line
+
+ 
+Average Salary of a teacher . . .. . . . .. . . . . .. .. $40,065
+
+ 
+Average Salary of a deployed Soldier . . .. . . .. $38,000
+
+ 
+I think we found where the cuts should be made! If you agree pass it on, as I just did.
+
+ 
+Warren Buffet, in a recent interview with CNBC, offers one of the best quotes about the debt ceiling:
+
+ 
+""I could end the deficit in five minutes,"" he told CNBC. ""You just pass a law that says that anytime there is a deficit of more than 3% of GDP, all sitting members of Congress are ineligible for re-election"".
+
+ 
+The 26th Amendment(granting the right to vote for 18 year-olds) took only three months and eight days to be ratified! Why? Simple! The people demanded it.That was in 1971 - before computers, e-mail, cell phones, etc.
+
+
+Of the 27 amendments to the Constitution, seven (7) took one(1) year or less to become the law of the land - all because of public pressure.
+
+
+Warren Buffet is asking each addressee to forward this email to a minimum of twenty people on their address list; in turn ask each of those to do likewise.
+
+
+In three days, most people in The United States of America will have the message.This is one idea that really should be passed around.
+
+
+Congressional Reform Act of 2015
+
+ 
+1. No Tenure / No Pension.  A Congressman/woman collects a salary while in office and receives no pay when they're out of office.
+
+ 
+2. Congress (past, present, & future) participates in Social Security.
+
+ 
+All funds in the Congressional retirement fund move to the Social Security system immediately. All future funds flow into the Social Security system, and Congress participates with the American people. It may not be used for any other purpose.
+
+ 
+3. Congress can purchase their own retirement plan, just as all Americans do.
+
+ 
+4. Congress will no longer vote themselves a pay raise. Congressional pay will rise by the lower of CPI or 3%.
+
+ 
+5. Congress loses their current health care system and participates in the same health care system as the American people.
+
+ 
+6. Congress must equally abide by all laws they impose on the American people.
+
+ 
+7. All contracts with past and present Congressmen/women are void effective 12/1/15. The American people did not make this contract with Congressmen/women.
+
+
+Congress made all these contracts for themselves.Serving in Congress is an honor, not a career.The Founding Fathers envisioned citizen legislators, so ours should serve their term(s), then go home and go back to work.
+
+
+If each person contacts a minimum of twenty people, then it will only take three days for most people (in the U.S.) to receive the message.Don't you think it's time?
+
+
+THIS IS HOW YOU FIX CONGRESS!
+
+
+If you agree, pass it on.If not, delete.
+";
+        #endregion
+
+    }
+}
---
+++ SearchEngine/DocumentLoader.cs
@@ -0,0 +1,88 @@
+﻿using AnalysisModel;
+using DataAccess.Interfaces;
+using SearchEngine.Interfaces;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchEngine
+{
+    /// <summary>
+    /// Responsible for loading raw documents into the corpus.
+    /// </summary>
+    public class DocumentLoaderAsync : IDocumentLoaderAsync
+    {
+        readonly int _blockSize;
+        readonly IRepositoryAsync<Document> _repository;
+
+        public DocumentLoaderAsync(IRepositoryAsync<Document> repository, int blockSize=4096)
+        {
+            _repository = repository;
+            _blockSize = blockSize;
+        }
+
+        /// <summary>
+        /// Utility method that fills a StringBuilder
+        /// </summary>
+        /// <param name="data">Stream containing document data.</param>
+        /// <returns>Content of the stream as a string.</returns>
+        private async Task<string> ReadStreamAsync(Stream data)
+        {
+            var buffer = new char[_blockSize];
+            var builder = new StringBuilder();
+
+            using (var reader = new StreamReader(data))
+            {
+                while (!reader.EndOfStream)
+                {
+                    await reader.ReadAsync(buffer, 0, _blockSize).ConfigureAwait(false);
+                    builder.Append(buffer);
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        public async Task<bool> LoadAsync(string path, string contentType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> LoadAsync(Uri resource)
+        {
+            using (var response = await WebRequest.Create(resource).GetResponseAsync().ConfigureAwait(false))
+            {
+                using (var stream = response.GetResponseStream())
+                {
+                    var content = await ReadStreamAsync(stream).ConfigureAwait(false);
+
+                    var document = new Document()
+                    {
+                        ContentType = response.ContentType,
+                        ContentLength = response.ContentLength,
+                        Content = content,
+                    };
+
+                    return await _repository.AddAsync(document).ConfigureAwait(false);
+                }
+            }
+        }
+
+        public async Task<bool> LoadAsync(Stream data, string contentType)
+        {
+            var content = await ReadStreamAsync(data).ConfigureAwait(false);
+
+            var document = new Document()
+            {
+                ContentType = contentType,
+                ContentLength = content.Length,
+                Content = content,
+            };
+
+            return await _repository.AddAsync(document).ConfigureAwait(false);
+        }
+
+    }
+}
